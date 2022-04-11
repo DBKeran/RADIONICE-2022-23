77713625d30364f4cc897e15783396c8339e5c96
Dino Keran <51824884+DBKeran@users.noreply.github.com>
2022-04-11 12:42:29 +0200

    Komentiranje radionice19

--- src/radionica19.h
+++ src/radionica19.h
@@ -2,55 +2,67 @@
 #include <helper.h>
 
 void RobotLine::radionica() {
+    // Uvođenje varijable lopta s kojom mjenjamo ponašanje robota ovisno dali drži robot loptu ili ne
     static bool lopta; 
     if (setup()){
         lopta = false; 
         armOpen();
     }
-
-    if (lopta == false){
+    
+    // Ukoliko robot NE nosi loptu ponašat će se prema sljedećim naredbama 
+    if (lopta == false){ 
         if(lineAny()){
+            // Priprema robota za praćenje linije
             armClose();
 
+            // Prepoznavanje raskrižja koji označava pronalazak lopte
             if (line(8) and line(4) and front() > 200){
                 catchBall();
                 lopta = true;
             }
 
+            // Praćenje linije
             lineFollow();
         } 
         else{
             // Priprema robota za praćenje zida
             armOpen();
 
+            // Praćenje zida
             wallFollowLeft();
             wallFollowRight();
         }
     }
+    
+    // Ukoliko robot NOSI loptu ponašat će se prema sljedećim naredbama    
     else{
         if(lineAny()){
+            // Prepozavanje raskrižja gdje robot ostavlja loptu
             if (line(8) and line(0) and front() < 200){
                 stop();
                 armDrop();
                 delayMs(100);
                 end();
-            }  
-              lineFollow();
+            }
+            
+            // Praćenje linije
+            lineFollow();
         }
         else{
-
+            // Praćenje zida dok drži loptu u gripperu
             wallFollowLeftCarry();
             wallFollowRightCarry();
         }
     }
 }
 
+
 void RobotLine::wallFollowRight() {
     // Praćenje zida s desne strane
     if(frontRight() < 200)
     {
         if (front() < 130){
-            go(-70, 70);
+            go(-70, 70);        // Skretanje u lijevu stranu
             delayMs(600);
         }
         if (frontRight() < 100){
@@ -64,11 +76,11 @@ void RobotLine::wallFollowRight() {
 }
 
 void RobotLine::wallFollowLeft() {
-    // Prioritet stavljen na praćenje zida s lijeve strane
+    // Praćenje zida s lijeve strane
     if (frontLeft() < 200)
     {   
         if (front() < 130){
-            go(70, -70);
+            go(70, -70);        // Skretanje u desnu stranu
             delayMs(600);
         }
         if (frontLeft() < 100){
@@ -77,37 +89,49 @@ void RobotLine::wallFollowLeft() {
         else
             go(30, 70);
     }
+    else
+        go(60, 60);
 }
 
 void RobotLine::catchBall() {
+    // Rutina za hvatanje lopte 
     stop();
     delayMs(50);
-    armCatchReady();
-    go(40, 40);
+    armCatchReady();        // Priprema grippera za hvatanje
+    
+    go(40, 40);             // Polako prilaženje loptici
     delayMs(2000);
     go(20, 20);
     delayMs(1000);
-    armCatch();
+    
+    armCatch();             // Hvatanje loptice
     stop();
     delayMs(50);
-    go(-40, -40);
-    delayMs(1500);
+    
+    go(-40, -40);           // Izlazak iz zone čuvanja loptice vožnjom unazad
+    delayMs(1500);  
     stop();
     delayMs(50);
-    go(70, -70);
-    delayMs(1300);
+    
+    go(70, -70);            // Okretanje u mjestu za 180° 
+    delayMs(1300);    
     stop();
     delayMs(50);
-    go(70, 70);
+    
+    go(70, 70);             // Prelazak raskrižja
     delayMs(500);
 }
 
+
+/*
+        OVAJ DIO MORAJU SAMI NAPRAVITI
+*/
 void RobotLine::wallFollowRightCarry() {
-    // Praćenje zida s desne strane
+    // Praćenje zida s desne strane nakon hvatanja lopte
     if(frontRight() < 200)
     {
         if (front() < 130){
-            go(-70, 70);
+            go(-70, 70);        // Skretanje u lijevu stranu
             delayMs(600);
         }
         if (frontRight() < 160){
@@ -121,11 +145,11 @@ void RobotLine::wallFollowRightCarry() {
 }
 
 void RobotLine::wallFollowLeftCarry() {
-    // Prioritet stavljen na praćenje zida s lijeve strane
+    // Praćenje zida s lijeve strane nakon hvatanja lopte
     if (frontLeft() < 200)
     {   
         if (front() < 130){
-            go(-70, 70);
+            go(-70, 70);        // Skretanje u lijevu stranu
             delayMs(600);
         }
         if (frontLeft() < 160){
@@ -134,4 +158,57 @@ void RobotLine::wallFollowLeftCarry() {
         else
             go(30, 70);
     }
+    else
+        go(60, 60);
+}
+
+
+/*
+        OVAJ DIO JE DODATNI ZA EXTRA BOD
+*/
+void RobotLine::findingBall() {
+    // Pakiranje dijela koda kada je lopta == false; 
+    if(lineAny()){
+        // Priprema robota za praćenje linije
+        armClose();
+        
+        // Prepoznavanje raskrižja koji označava pronalazak lopte
+        if (line(8) and line(4) and front() > 200){
+            catchBall();
+            lopta = true;
+        }
+        
+        // Praćenje linije
+        lineFollow();
+    } 
+    else{
+        // Priprema robota za praćenje zida
+        armOpen();
+
+        // Praćenje zida
+        wallFollowLeft();
+        wallFollowRight();
+    }
+}
+
+void RobotLine::carryingBall() {
+    // Pakiranje dijela koda kada je lopta == true; 
+    // Ukoliko robot NOSI loptu ponašat će se prema sljedećim naredbama 
+    if(lineAny()){
+        // Prepozavanje raskrižja gdje robot ostavlja loptu
+        if (line(8) and line(0) and front() < 200){
+            stop();
+            armDrop();
+            delayMs(100);
+            end();
+        }
+
+        // Praćenje linije
+        lineFollow();
+    }
+    else{
+        // Praćenje zida dok drži loptu u gripperu
+        wallFollowLeftCarry();
+        wallFollowRightCarry();
+    }    
 }
