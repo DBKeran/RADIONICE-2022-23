2b8f0b7cad5dbd5d11d3129f30d85c329b2fbc3f
Dino Keran <dino.bucevic.keran@gmail.com>
2022-09-18 12:01:05 +0200

    empty radionica

--- src/radionica.h
+++ src/radionica.h
@@ -2,25 +2,5 @@
 #include "helper.h"
 
 void RobotLine::radionica() {
-	static int linesNoticed = 0;
-	static int speed = 50;
-	
-	if (linesNoticed == 3){
-		stop();
-		delayMs(1000);
-		go(-50, -50);
-		delayMs(1000);
-		speed = -50;
-		linesNoticed++;
-	}
 
-	if (linesNoticed == 5)
-		speed = 0;
-	
-	go(speed, speed);
-	
-	if (lineAny()){
-		linesNoticed++;
-		delayMs(500);
-	}
 }
