bbb46b1d68a3b02f0678cb2f6fb038c00fcc59a3
Dino Keran <51824884+DBKeran@users.noreply.github.com>
2022-09-30 13:26:08 +0200

    Create prikaz-brojaca.h

---
+++ src/2022_SPASAVANJE/prikaz-brojaca.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "helper.h"
+void RobotLine::radionica() {
+// ZADATAK: uvedite varijablu brojača. 
+// HINT: static int nazivVarijable = vrijednost;
+ static int broj = 0;
+
+// VARIJABLA ZA PRETVORBU BROJA U TEKST
+ char simbol[10 + sizeof(char)];
+
+// ZADATAK: skretanje s povećanjem brojača
+ if (front() < 120){
+   go(-50, 50);
+   delayMs(600);
+   broj++;
+ }
+ if (rightFront() > 120)
+   go(80, 20);
+ else
+   go(20, 80);
+
+// PRETVORBA VARIJABLE BROJAČA U TEKST
+ sprintf(simbol,"%d", broj);
+// PRIKAZ PRETVORENE VARIJABLE SPREMNE ZA BROJAČ
+ display(simbol);
+}
