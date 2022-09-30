a0e0cd4e709dc6e1315b96dcd271afc59f462506
Dino Keran <51824884+DBKeran@users.noreply.github.com>
2022-09-30 13:13:10 +0200

    Update display-variables.h

--- src/tips-tricks/display-variables.h
+++ src/tips-tricks/display-variables.h
@@ -2,15 +2,25 @@
 #include "helper.h"
  
 void RobotLine::radionica() {
-static int i = 0;
-char i_char[10 + sizeof(char)];
+// ZADATAK: uvedite varijablu brojača. 
+// HINT: static int nazivVarijable = vrijednost;
+ static int broj = 0;
+// VARIJABLA ZA PRETVORBU BROJA U TEKST
+ char simbol[10 + sizeof(char)];
 
-if (front() < 100)
-    i++;
-
-sprintf(i_char,"%d", i);
-
-display(i_char);
-delayMs(500);
-
+// ZADATAK: skretanje s povećanjem brojača
+ if (front() < 120){
+   go(-50, 50);
+   delayMs(600);
+   broj++;
+ }
+ if (rightFront() > 120)
+   go(80, 20);
+ else
+   go(20, 80);
+ }
+// PRETVORBA VARIJABLE BROJAČA U TEKST
+ sprintf(simbol,"%d", broj);
+// PRIKAZ PRETVORENE VARIJABLE SPREMNE ZA BROJAČ
+ display(simbol);
 }
