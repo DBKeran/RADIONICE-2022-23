a538d66cd505acaf974acf74de836212a088c812
Dino Keran <51824884+DBKeran@users.noreply.github.com>
2022-11-05 09:28:56 +0100

    Update 1_grananje-if-broj.h

--- src/2022_SPASAVANJE/5_RADIONICE/1_grananje-if-broj.h
+++ src/2022_SPASAVANJE/5_RADIONICE/1_grananje-if-broj.h
@@ -1,23 +1,46 @@
 #pragma once
 #include "helper.h"
 void RobotLine::radionica() {
+  if(setup()){
+    armOpen();
+  }
+  // ZADATAK: uvedite varijablu brojača.
+  // HINT: static int nazivVarijable = vrijednost;
   static int broj = 0;
+
   // VARIJABLA ZA PRETVORBU BROJA U TEKST
   char simbol[10 + sizeof(char)];
 
-  // ZADATAK: skretanje s povećanjem brojača
-  if (front() < 120) {
-    go(-90, 90);
-    delayMs(500);
-    broj++;
+  if (lineAny()) {
+    lineFollow();
   }
-  if (rightFront() > 120)
-    go(80, 20);
-  else
-    go(20, 80);
-
-  // PRETVORBA VARIJABLE BROJAČA U TEKST
+  else {
+    // ZADATAK: skretanje s povećanjem brojača
+    if (front() < 120) {
+      go(90, -90);
+      delayMs(500);
+      broj++;
+    }
+    // ZADATAK: Dodajte praćenje lijevog zida uz postojeće pračenje desnog zida.
+    // HINT: Koristite grananje if uvjeta
+    else if (rightFront() < leftFront()) {
+      if (rightFront() < 120) {
+        if (rightFront() > 100)
+          go(80, 20);
+        else
+          go(20, 80);
+      }
+    }
+    else if (leftFront() < 120) {
+      if (leftFront() > 100)
+        go(20, 80);
+      else
+        go(80, 20);
+    }
+    else
+      go(50, 50);
+  }
+  // Ne mjenjati, služi za pretvorbu vrste varijable i ispis na 8x8 led
   sprintf(simbol, "%d", broj);
-  // PRIKAZ PRETVORENE VARIJABLE SPREMNE ZA BROJAČ
   display(simbol);
 }
